59ab2c4c4bfeb49c04943bcf43df525674a134fd
MrMa-rio <112214759+MrMa-rio@users.noreply.github.com>
2023-04-03 20:19:08 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,22 @@ import ReactDOM from 'react-dom/client'
 import Home from './Pages/Home/Home'
 import { Search } from './components/index'
 import {Upcoming, Popular, Top, Detail, NotFound} from '../src/Pages/index'
-import { HashRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <div className='w-screen'>
+  <>
+    <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}  />
-          <Route path='/popular' element={<Popular name='Popular' />} />
-          <Route path='/upcoming' element={<Upcoming name='Upcoming_Movies' />} />
-          <Route path='/top' element={<Top name='Top Movies' />} />
+          <Route path='/popular' element={<Popular />} />
+          <Route path='/upcoming' element={<Upcoming />} />
+          <Route path='/top' element={<Top />} />
           <Route path='/details/:searchId?/:id' element={<Detail />} />
           <Route path='/search/:id' element={<Search />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
-      </div>
-    </Router>
-  </React.StrictMode>,
+    </BrowserRouter>
+  </>,
 )
